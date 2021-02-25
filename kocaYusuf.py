050009a644a2bc750d89413a73ba4c23195bbec3
kaankaray <kaankaray99@gmail.com>
2021-02-25 12:36:54 +0200

    repo checks

--- kocaYusuf.py
+++ kocaYusuf.py
@@ -1,15 +1,64 @@
-import keyboard
-import pymem
-import pymem.process
-import time
-import re
-from time import sleep
 import os
-from termcolor import colored, cprint
-import requests
-from datetime import datetime
+try:
+  import keyboard 
+except ImportError:
+  print("A module is not existing. Installing module: keyboard")
+  os.system("python -m pip install keyboard")
+  import keyboard
 
-from win32gui import GetWindowText, GetForegroundWindow
+try:
+  import pymem 
+  import pymem.process
+except ImportError:
+  print("A module is not existing. Installing module: pymem")
+  os.system("python -m pip install pymem")
+  import pymem
+  import pymem.process
+  
+try:
+  import time 
+  from time import sleep
+except ImportError:
+  print("A module is not existing. Installing module: time")
+  os.system("python -m pip install time")
+  import time
+  from time import sleep
+  
+try:
+  import re 
+except ImportError:
+  print("A module is not existing. Installing module: re")
+  os.system("python -m pip install re")
+  import re
+  
+try:
+  from termcolor import colored, cprint 
+except ImportError:
+  print("A module is not existing. Installing module: termcolor")
+  os.system("python -m pip install termcolor")
+  from termcolor import colored, cprint 
+
+try:
+  import requests 
+except ImportError:
+  print("A module is not existing. Installing module: requests")
+  os.system("python -m pip install requests")
+  import requests
+  
+try:
+  from datetime import datetime
+except ImportError:
+  print("A module is not existing. Installing module: datetime")
+  os.system("python -m pip install datetime")
+  from datetime import datetime
+
+try:
+  from win32gui import GetWindowText, GetForegroundWindow
+except ImportError:
+  print("A module is not existing. Installing module: pywin32")
+  os.system("python -m pip install pywin32")
+  from win32gui import GetWindowText, GetForegroundWindow
+  
 
 url = 'https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json'
 response = requests.get(url).json()
